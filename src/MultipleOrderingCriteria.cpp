5a5427aeb3680d130abf953e4a11d1eaf974355a
r-tarnowski <radek.github@wp.pl>
2023-07-09 22:44:39 +0200

    Example of using std::common_comparison_category added

--- src/MultipleOrderingCriteria.cpp
+++ src/MultipleOrderingCriteria.cpp
@@ -82,6 +82,45 @@ private:
 };
 
 
+class PersonCommonComparisonCategory {
+
+private:
+   // Must be declared before using decltype !!!
+   double value;
+   std::string firstName;
+   std::string lastName;
+
+public:
+
+   explicit PersonCommonComparisonCategory( double valId, const char * pFirstName, const char * pLastName )
+         :  value { valId },
+            firstName( pFirstName ),
+            lastName( pLastName ) {
+   }
+
+   PersonCommonComparisonCategory() = delete;
+   ~PersonCommonComparisonCategory() = default;
+
+   auto operator <=> ( const PersonCommonComparisonCategory & rhs ) const ->
+      std::common_comparison_category_t< decltype( firstName <=> rhs.firstName ) , decltype( value <=> rhs.value )> {
+
+      auto cmp1 = lastName <=> rhs.lastName;
+      if ( cmp1 != 0 ) {
+         return cmp1; //used as or converted to commoncomparison type
+      }
+
+      auto cmp2 = firstName <=> rhs.firstName;
+      if ( cmp2 != 0 ) {
+         return cmp2;
+      }
+
+      return value <=> rhs.value; //used as or converted to commoncomparison type
+   }
+
+};
+
+
+
 
 int main( int argc, char *argv[] ) {
     printHeader();
@@ -106,6 +145,17 @@ int main( int argc, char *argv[] ) {
              << ( std::partial_ordering::equivalent == donaldWeakest <=> johnnyWeakest )  << std::endl;
    std::cout << "( std::partial_ordering::greater == donaldWeakest <=> johnnyWeakest ) -> " << std::boolalpha
              << ( std::partial_ordering::greater == donaldWeakest <=> johnnyWeakest )  << std::endl;
+
+   cout << "---------------------------------------------------------------------------------" << endl;
+   PersonCommonComparisonCategory donaldCCC( 102.1,  "Donald", "Duck" );
+   PersonCommonComparisonCategory johnnyCCC( 101.1,  "Johnny", "Walker" );
+
+   std::cout << "( std::partial_ordering::less == donaldCCC <=> johnnyCCC ) -> " << std::boolalpha
+             << ( std::partial_ordering::less == donaldCCC <=> johnnyCCC )  << std::endl;
+   std::cout << "( std::partial_ordering::equivalent == donaldCCC <=> johnnyCCC ) -> " << std::boolalpha
+             << ( std::partial_ordering::equivalent == donaldCCC <=> johnnyCCC )  << std::endl;
+   std::cout << "( std::partial_ordering::greater == donaldCCC <=> johnnyCCC ) -> " << std::boolalpha
+             << ( std::partial_ordering::greater == donaldCCC <=> johnnyCCC )  << std::endl;
 
 
     return 0;
